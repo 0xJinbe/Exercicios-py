f87c50fa9c76623d21a00de06b066a2d35f70f90
Claudio W. Germano Jr <80705963+claudio-germano@users.noreply.github.com>
2021-04-07 09:07:53 -0300

    Estrutura de decisão

---
+++ Ex 007.py
@@ -0,0 +1,9 @@
+"""Faça um Programa que peça um valor e mostre na tela se o valor é positivo ou negativo.
+"""
+
+valor = float(input('Entre com um valor: '))
+
+if valor > 0:
+    print('Valor é positivo')
+else:
+    print('Valor é negativo')
