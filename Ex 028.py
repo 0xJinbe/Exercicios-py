ee8d54b02ea9a115d6fac21534b8ee5a2741dc54
claudio-germano <wantuir.germano@gmail.com>
2021-04-12 12:45:16 -0300

    Estrutura de decisão

---
+++ Ex 028.py
@@ -0,0 +1,8 @@
+"""Faça um programa que peça um numero inteiro e determine se ele é par ou impar. Utilize operador de modulo"""
+
+num = int(input('Entre com o numero: '))
+
+if num % 2 == 0:
+    print('Numero par')
+else:
+    print('Número ímpar...')
